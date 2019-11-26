6b72026195ad73115c42ff48f009f33a2a680cc1
Martha <Arnaud>
2019-11-26 04:06:10 -0600

    guardar dinero actual desde pantalla de inicio

--- Apis/Apis/Controllers/IngresoInicialViewController.swift
+++ Apis/Apis/Controllers/IngresoInicialViewController.swift
@@ -12,25 +12,30 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var nombre: UITextField!
     @IBOutlet weak var salario: UITextField!
+    @IBOutlet weak var dineroActual: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let salario = UserDefaults.standard.value(forKey: "Salario"), let nombre = UserDefaults.standard.value(forKey: "Nombre") as? String {
+        if let salario = UserDefaults.standard.value(forKey: "Salario"), let nombre = UserDefaults.standard.value(forKey: "Nombre") as? String, let dineroActual = UserDefaults.standard.value(forKey: "DineroActual")  {
             self.salario.text = "\(salario)"
             self.nombre.text = nombre
+            self.dineroActual.text = "\(dineroActual)"
+
+            }
         }
         
-    }
+    
     
     @IBAction func registraPagosFijos(_ sender: UIButton) {
-        guard let salario = Double(self.salario.text!), let nombre = self.nombre.text else {
+        guard let salario = Double(self.salario.text!), let nombre = self.nombre.text, let dineroActual = Double(self.dineroActual.text!) else {
             //mostrar alerta
             return
         }
         
         UserDefaults.standard.set(salario, forKey: "Salario")
         UserDefaults.standard.set(nombre, forKey: "Nombre")
+        UserDefaults.standard.set(dineroActual, forKey: "DineroActual")
     }
 }
 
